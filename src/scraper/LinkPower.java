0978c65e519c4dc3a07a162e84a1a71dd89732c6
bbak0 <b_bak@outlook.com>
2017-06-04 15:16:52 +0200

    Now saves power and loyalty while looking for links because that info is not available in subpages.

---
+++ src/scraper/LinkPower.java
@@ -0,0 +1,21 @@
+package scraper;
+
+// Because gwentdb doesnt show power on individual card pages it needs to be
+// saved while looking for individual links. This small class will hold pairs
+// of link to card and its power.
+
+//added Loyal stat
+
+public class LinkPower {
+	
+	String link;
+	int power;
+	Boolean loyal; //nullable boolean because its not applicable to some cards
+	
+	public LinkPower(String link, int power, Boolean loyal){
+		this.link = link;
+		this.power = power;
+		this.loyal = loyal;
+	}
+
+}
--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -2,23 +2,23 @@ package scraper;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import java.sql.*;	
 
 public class Scraper {
 	
-	List<String> cardPages = new ArrayList<String>();
+	ArrayList<String> cardPages = new ArrayList<String>();
+	ArrayList<Integer> cardPower = new ArrayList<Integer>();
+	ArrayList<LinkPower> cardPagesWithPower = new ArrayList<LinkPower>();
 	
 	ArrayList<Card> cardList = new ArrayList<Card>();
 	
 	final String startSite = "http://www.gwentdb.com/cards?filter-display=1";
 	final String baseURL = "http://www.gwentdb.com/cards?filter-display=1&page=";
-	final String baseCardURL = "http://www.gwentdb.com/cards/";
+	final String baseCardURL = "http://www.gwentdb.com/cards";
 	final boolean getHighResImages = false;
 	String dbPath;
 	String dbName;
@@ -44,23 +44,57 @@ public class Scraper {
 				//append page number to the base url and get the elements with card-row class
 				String currentURL = baseURL + i;
 				Document currentPage = Jsoup.connect(currentURL).get();
-				Elements links = currentPage.select(".col-name");
+				Elements links = currentPage.select(".card-row");
 				
 				// first element is table header therefore it is removed
-				links.remove(0);
+				//links.remove(0);
 				
-				//there will be around 20 elements per page, each represents a card
-				//below the link for each page is extracted
+				// there will be around 20 elements per page, each represents a card
+				// below the link for each page is extracted
 				for (Element x : links){
 					String innerHTML = x.html();
+					
+					// gets the page link
 					int startIndex = innerHTML.indexOf("a href=\"/cards") + "a href=\"/cards".length();
 					int endIndex = innerHTML.indexOf("\"", startIndex);
 					String adress = innerHTML.substring(startIndex, endIndex);
-					cardPages.add(adress);
+					
+					//gets card's power
+					startIndex = innerHTML.indexOf("<td class=\"col-power\">") + "<td class=\"col-power\">".length();
+					endIndex =  innerHTML.indexOf("<", startIndex);
+					int power = Integer.valueOf(innerHTML.substring(startIndex, endIndex));
+					
+					//gets card's loyalty
+					Boolean loyalty = null;
+					if (innerHTML.contains("is-disloyal tip")){
+						loyalty = false;
+					} else if (innerHTML.contains("is-loyal tip")){
+						loyalty = true;
+					}
+					
+					cardPagesWithPower.add(new LinkPower(adress,power,loyalty));
 				}
 				
 			}
 			System.out.println("done");
+	}
+	
+	void getAllCardDefinitions() throws IOException{
+		
+		for (String page : cardPages){
+			
+			// Generate proper link to the card page and download its contents
+			String fullLink = baseCardURL + page;
+			Document currentWebsite = Jsoup.connect(fullLink).get();
+			
+			// Get values for card;
+			
+			
+			// Create and fill empty card definition
+			
+			
+		}
+		
 	}
 	
 	void createNewDatabase(){
